d7e88ee53ec4119d9677456a10df1f2be4a0b9a0
Ariel Rojas <arielalejandrorojas@gmail.com>
2026-04-20 14:00:10 -0400

    Remove Firebase keys from tracked environment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,13 +2,13 @@ export const environment = {
   production: false,
   appName: 'Latido',
   firebase: {
-    apiKey: 'AIzaSyAtGeYClWA8kXZBG4z4j1UBucN68FWW-Sw',
-    authDomain: 'latido-app-546ef.firebaseapp.com',
-    projectId: 'latido-app-546ef',
-    storageBucket: 'latido-app-546ef.firebasestorage.app',
-    messagingSenderId: '372701179179',
-    appId: '1:372701179179:web:11e2c7520fb48db908a96e',
+    apiKey: 'REPLACE_FIREBASE_API_KEY',
+    authDomain: 'REPLACE_FIREBASE_AUTH_DOMAIN',
+    projectId: 'REPLACE_FIREBASE_PROJECT_ID',
+    storageBucket: 'REPLACE_FIREBASE_STORAGE_BUCKET',
+    messagingSenderId: 'REPLACE_FIREBASE_MESSAGING_SENDER_ID',
+    appId: 'REPLACE_FIREBASE_APP_ID',
     vapidKey: '',
-    measurementId: 'G-RC7X0MCS1E'
+    measurementId: ''
   }
 };
